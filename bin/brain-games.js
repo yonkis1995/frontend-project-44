fa2d93ecbf651cdd72c5f69f03aecc86dbccbbad
Viktor Yevseyev <yonkis1995@gmail.com>
2025-10-11 00:59:24 +0300

    style: format code with ESlint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { greetUserByName } from "../src/cli.js";
+import { greetUserByName } from '../src/cli.js'
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!')
 
-greetUserByName();
+greetUserByName()
