4fb9fa4719621619e435f9c2eb20275f7dadc22c
Viktor Yevseyev <yonkis1995@gmail.com>
2025-10-18 14:50:19 +0300

    feat: add brain-gcd game

---
+++ src/games/gcdGame.js
@@ -0,0 +1,18 @@
+const gameDescription = 'Find the greatest common divisor of given numbers.'
+
+const getRandomNum = maxNum => Math.floor(Math.random() * maxNum)
+const gcd = (num1, num2) => (num2 === 0) ? num1 : gcd(num2, num1 % num2)
+
+const generateData = () => {
+  const generatedData = {} // объект для хранения вопроса и ответа
+
+  const randomNum1 = getRandomNum(100)
+  const randomNum2 = getRandomNum(100)
+
+  generatedData['question'] = `${randomNum1} ${randomNum2}`
+  generatedData['answer'] = gcd(randomNum1, randomNum2).toString()
+
+  return generatedData
+}
+
+export { gameDescription, generateData }
